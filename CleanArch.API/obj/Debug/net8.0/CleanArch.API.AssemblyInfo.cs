838e572a3c5697affd4cd2e24b56df01ba7485e6
mrehaniqbal01@gmail.com <mrehaniqbal01@gmail.com>
2024-10-03 04:11:51 +0500

    Country-crudd

--- CleanArch.API/obj/Debug/net8.0/CleanArch.API.AssemblyInfo.cs
+++ CleanArch.API/obj/Debug/net8.0/CleanArch.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArch.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08f3718297e6306b382ed38f9dc0a368e9c1085b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArch.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArch.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
